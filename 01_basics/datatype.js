fe4c4a7dca427209f497288d4d5d04919dc657ce
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2025-03-16 13:37:57 +0000

    datatype

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -21,30 +21,4 @@ console.log(typeof "pallavi") //string
 console.log(typeof 234)   // number
 console.log(typeof null)   // object
 console.log(typeof undefined)  // undefined
-console.log(typeof symbol)    //undefined
-
-
-/// dataype conversion 
-let score=true;
-console.log(typeof(score));
-// to want in number
-let valno=Number(score)
-console.log(typeof(score));
-console.log(valno)
-
-//number-get that number only
-//string-nan
-//null - 0
-//undefined- nan
-//boolean-1
-
-let str="pallavi"
-let  boolvalue=Boolean(str)
-console.log(boolvalue)
-//""=> false
-//"pallavi"=>true
-
-let num= true
-let strvalue=String(num)
-console.log(typeof strvalue)
-console.log(strvalue)
+//console.log(typeof symbol)    //undefined
---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,41 @@
+/// dataype conversion 
+let score=true;
+// console.log(typeof(score));
+// to want in number
+let valno=Number(score)
+// console.log(typeof(score));
+// console.log(valno)
+
+//number-get that number only
+//string-nan
+//null - 0
+//undefined- nan
+//boolean-1
+
+let str="pallavi"
+let  boolvalue=Boolean(str)
+// console.log(boolvalue)
+//""=> false
+//"pallavi"=>true
+
+let num= true
+let strvalue=String(num)
+// console.log(typeof strvalue)
+// console.log(strvalue)
+
+////************Operation ********* */
+
+let n=3;
+let nev=-n
+console.log(nev);
+
+console.log("1"+2);
+console.log(1+2);
+console.log(1+"2");
+console.log("1"+2+"1");
+console.log(1+2+"1");    /// it is the rare case 
+
+console.log(+true)
+console.log(+"")
+console.log(+11)
+
