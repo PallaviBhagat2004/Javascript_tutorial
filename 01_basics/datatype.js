756a59b58ab663f2a30fcc56e2d2b06ccaff01b2
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2025-03-16 11:05:21 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict";   /// treat all the js code as the newer version 
+
+//alert("hello")    // we use this in the browser not in node js 
+
+/// code ki readability maintain karna jaurri hai
+
+// datatype in js
+/*
+        Primitive datatype
+1.number  have the range 2  power 53
+2.big int is used to store the large number 
+3.string which can stored in single aor double quotes but prefered double quotes
+4. boolean used to store true or false
+5. null is standalone value  (empty value)
+6. undefined -- value intializw nhi kiya hai 
+7. symbol --- uniquessness defined karne ke liye we use this 
+
+object 
+ */
+console.log(typeof "pallavi") //string
+console.log(typeof 234)   // number
+console.log(typeof null)   // object
+console.log(typeof undefined)  // undefined
+
