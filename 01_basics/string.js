c8e281e6ba7a63ebfee6b06704d3355f3f26a544
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2025-03-18 18:24:40 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,24 @@
+const str="mango is the king of the fruit "
+const repo="234"
+// console.log(name+repo+" hello welcome to your class");     we don't use this method 
+console.log(`The ${str} repo is ${repo} hello welcome to your class`);
+
+///new declaration of the string
+const str1=new String('  pallavi   ')   // in these the type of the str is object 
+console.log(str);
+
+console.log(str[0]);
+console.log(str.__proto__);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(2));
+console.log(str.indexOf('a'));
+console.log(str.repeat(3));
+console.log(str.replace('go','ho'));
+console.log(str1.trim());
+console.log(str.includes('mango'));
+console.log(str.slice(0,4));
+console.log(str.split('-'));
+
+
+
